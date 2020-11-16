85ffff0e690e5851d4a58cafb0449bddfdee8d81
Fabian Wenzelmann <fabian.wenzelmann@adition.com>
2020-11-16 12:14:44 +0100

    Ensure logging is visible

--- app/main.py
+++ app/main.py
@@ -1,4 +1,17 @@
+import logging
+
 from fastapi import FastAPI
+from fastapi.logger import logger
+
+# make sure logging is also available in production
+# see <https://github.com/tiangolo/uvicorn-gunicorn-fastapi-docker/issues/19>
+gunicorn_logger = logging.getLogger('gunicorn.error')
+logger.handlers = gunicorn_logger.handlers
+
+if __name__ != "main":
+    logger.setLevel(gunicorn_logger.level)
+else:
+    logger.setLevel(logging.DEBUG)
 
 app = FastAPI()
 
