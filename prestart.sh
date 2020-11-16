6812e6449e7228e89e90cf275149cf2773600d56
Fabian Wenzelmann <fabian.wenzelmann@adition.com>
2020-11-16 13:26:10 +0100

    Add files / instructions we need to use prometheus multiprocess mode

---
+++ prestart.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env sh
+
+# This script is executed by the docker image before the actual job starts
+# We use this to make sure that the prometheus-tmp directory is empty
+
+if [ -z "${prometheus_multiproc_dir}" ]; then
+  echo "environment variable prometheus_multiproc_dir is not set but required for prometheus multiprocess mode!"
+  exit 1
+fi
+
+if [ ! -d "${prometheus_multiproc_dir}" ]; then
+  printf "prometheus_multiproc_dir (\"%s\") is not a directory\n" "${prometheus_multiproc_dir}"
+  exit 1
+fi
+
+# cleanup the prometheus-tmp dir
+rm -rf "${prometheus_multiproc_dir}/*"
+printf "prometheus_multiproc_dir (\"%s\") has been wiped clean\n" "${prometheus_multiproc_dir}"
+
+cat /gunicorn_conf.py
