39efe3fe58128640116d59c6a8b08907e8e3da4d
Moritz Degott <moritz.degott@adition.com>
2020-11-24 18:05:55 +0100

    Use correct timedate objects in tests

--- tests/hive_impala_query_builder_test.py
+++ tests/hive_impala_query_builder_test.py
@@ -64,7 +64,7 @@ def test_runtime_one_day():
                "(`year` = 2017 AND `month` = 5 AND `day` = 14 AND `hour` BETWEEN 0 AND 21)" \
                ")"
     assert query_builder.build_partition_filter() == expected
-    assert query_builder.build_timestamp_filter() == "`timestamp` BETWEEN 1494709200 AND 1494795599"
+    assert query_builder.build_timestamp_filter() == "`timestamp` BETWEEN 1494712800 AND 1494799199"
     assert str(query_builder.end_date) == "2017-05-14 21:59:59+00:00"
     assert str(query_builder.start_date) == "2017-05-13 22:00:00+00:00"
 
@@ -74,8 +74,8 @@ def test_runtime_two_days():
     Test the partitions if the report timerange has two days.
     """
     query_builder = PartitionQueryBuilder(
-        parser.parse("2017-05-13 00:00:00").astimezone(tz.tzutc()),
-        parser.parse("2017-05-14 23:59:59").astimezone(tz.tzutc())
+        datetime(year=2017, month=5, day=12, hour=22, tzinfo=pytz.utc),
+        datetime(year=2017, month=5, day=14, hour=21, minute=59, second=59, tzinfo=pytz.utc)
     )
     expected = "(" \
                "(`year` = 2017 AND `month` = 5 AND `day` = 12 AND `hour` BETWEEN 22 AND 23)" \
@@ -85,7 +85,7 @@ def test_runtime_two_days():
                "(`year` = 2017 AND `month` = 5 AND `day` = 14 AND `hour` BETWEEN 0 AND 21)" \
                ")"
     assert query_builder.build_partition_filter() == expected
-    assert query_builder.build_timestamp_filter() == "`timestamp` BETWEEN 1494622800 AND 1494795599"
+    assert query_builder.build_timestamp_filter() == "`timestamp` BETWEEN 1494626400 AND 1494799199"
     assert str(query_builder.end_date) == "2017-05-14 21:59:59+00:00"
     assert str(query_builder.start_date) == "2017-05-12 22:00:00+00:00"
 
@@ -95,8 +95,8 @@ def test_runtime_one_month():
     Test the partitions if the start date and end date are at different months.
     """
     query_builder = PartitionQueryBuilder(
-        parser.parse("2017-04-13 00:00:00").astimezone(tz.tzutc()),
-        parser.parse("2017-05-14 23:59:59").astimezone(tz.tzutc())
+        datetime(year=2017, month=4, day=12, hour=22, tzinfo=pytz.utc),
+        datetime(year=2017, month=5, day=14, hour=23,minute=59, second=59, tzinfo=pytz.utc)
     )
     expected = "(" \
                "(`year` = 2017 AND `month` = 4 AND `day` = 12 AND `hour` BETWEEN 22 AND 23)" \
@@ -105,7 +105,7 @@ def test_runtime_one_month():
                " OR " \
                "(`year` = 2017 AND `month` = 5 AND `day` BETWEEN 1 AND 13)" \
                " OR " \
-               "(`year` = 2017 AND `month` = 5 AND `day` = 14 AND `hour` BETWEEN 0 AND 21)" \
+               "(`year` = 2017 AND `month` = 5 AND `day` = 14 AND `hour` BETWEEN 0 AND 23)" \
                ")"
     assert query_builder.build_partition_filter() == expected
 
@@ -115,8 +115,8 @@ def test_runtime_two_months():
     Test the partitions for more than one month.
     """
     query_builder = PartitionQueryBuilder(
-        parser.parse("2017-03-13 00:00:00").astimezone(tz.tzutc()),
-        parser.parse("2017-05-14 23:59:59").astimezone(tz.tzutc())
+        datetime(year=2017, month=3, day=12, hour=23, tzinfo=pytz.utc),
+        datetime(year=2017, month=5, day=14, hour=23, minute=59, second=59, tzinfo=pytz.utc)
     )
     expected = "(" \
                "(`year` = 2017 AND `month` = 3 AND `day` = 12 AND `hour` = 23)" \
@@ -127,40 +127,7 @@ def test_runtime_two_months():
                " OR " \
                "(`year` = 2017 AND `month` = 5 AND `day` BETWEEN 1 AND 13)" \
                " OR " \
-               "(`year` = 2017 AND `month` = 5 AND `day` = 14 AND `hour` BETWEEN 0 AND 21)" \
-               ")"
-    assert query_builder.build_partition_filter() == expected
-
-
-def test_summer_wintertime():
-    """
-    Test if the partitions are calculated correctly during summer winter time change.
-    """
-    query_builder = PartitionQueryBuilder(
-        parser.parse("2017-10-29 00:00:00").astimezone(tz.tzutc()),
-        parser.parse("2017-10-29 23:59:59").astimezone(tz.tzutc())
-    )
-    expected = "(" \
-               "(`year` = 2017 AND `month` = 10 AND `day` = 28 AND `hour` BETWEEN 22 AND 23)" \
-               " OR " \
-               "(`year` = 2017 AND `month` = 10 AND `day` = 29 AND `hour` BETWEEN 0 AND 22)" \
-               ")"
-    assert query_builder.build_partition_filter() == expected
-
-
-def test_winter_summertime():
-    """
-    Test if the partitions are calculated correctly during winter summer time change.
-    """
-
-    query_builder = PartitionQueryBuilder(
-        parser.parse("2017-03-26 00:00:00").astimezone(tz.tzutc()),
-        parser.parse("2017-03-26 23:59:59").astimezone(tz.tzutc())
-    )
-    expected = "(" \
-               "(`year` = 2017 AND `month` = 3 AND `day` = 25 AND `hour` = 23)" \
-               " OR " \
-               "(`year` = 2017 AND `month` = 3 AND `day` = 26 AND `hour` BETWEEN 0 AND 21)" \
+               "(`year` = 2017 AND `month` = 5 AND `day` = 14 AND `hour` BETWEEN 0 AND 23)" \
                ")"
     assert query_builder.build_partition_filter() == expected
 
@@ -170,8 +137,8 @@ def test_more_than_one_year():
     Test if the partitions are calculated correctly when the time difference is more than two years.
     """
     query_builder = PartitionQueryBuilder(
-        parser.parse("2014-01-26 00:00:00").astimezone(tz.tzutc()),
-        parser.parse("2017-05-26 23:59:59").astimezone(tz.tzutc())
+        datetime(year=2014, month=1, day=25, hour=23, tzinfo=pytz.utc),
+        datetime(year=2017, month=5, day=26, hour=21, minute=59, second=59, tzinfo=pytz.utc)
     )
 
     expected = "(" \
