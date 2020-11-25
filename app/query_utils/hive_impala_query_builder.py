72ab63135851d948951f73f6abaabaed2cb51c62
sseipp <sebastian.seipp@adition.com>
2020-11-25 16:20:49 +0100

    remove unnecessary method

--- app/query_utils/hive_impala_query_builder.py
+++ app/query_utils/hive_impala_query_builder.py
@@ -34,20 +34,6 @@ def generate_timerange_query(start: datetime, end: datetime) -> str:
     return "{0} AND {1}".format(time_filter, partition_filter)
 
 
-def is_first_date_time_greater(first_date_time: datetime, second_date_time: datetime) -> bool:
-    """
-    Returns True if the first datetime has a bigger timestamp than the second datetime.
-
-    Args:
-        first_date_time: First datetime.
-        second_date_time: Second datetime.
-
-    Returns:
-        True if the first date is after the second date, False otherwise.
-    """
-    return first_date_time.timestamp() > second_date_time.timestamp()
-
-
 class PartitionQueryBuilder(object):
     """
     Generates the partition string for the given start and end dates.
