5769bc5e816505fc01d39627b39a1d21b75fb586
sseipp <sebastian.seipp@adition.com>
2020-11-25 12:56:28 +0100

    use pydantics dataclass for TimeRangeContainer, add validation and add unit tests for validation

--- app/query_utils/time_range_container.py
+++ app/query_utils/time_range_container.py
@@ -1,34 +1,51 @@
 from typing import List, Optional
-from pydantic import BaseModel, validator
+from pydantic.dataclasses import dataclass
+from pydantic import validator
 
 
-class TimeRangeContainer(BaseModel):
+@dataclass
+class TimeRangeContainer(object):
     """
     Container holding information about the different years, months, days and hours that need to be considered to
     generate the partition queries for a given time range.
     """
     years: List[int]
-    months: Optional[List[int]]
-    days: Optional[List[int]]
-    hours: Optional[List[int]]
+    months: Optional[List[int]] = None
+    days: Optional[List[int]] = None
+    hours: Optional[List[int]] = None
 
+    @validator('years')
+    def years_must_contain_values(cls, v):
+        if not v:
+            raise ValueError('A time range container always requires years.')
+        return v
 
-@validator('years')
-def years_must_contain_values(cls, v):
-    if not len(v):
-        raise ValueError('A time range container always requires years')
-    return v
+    @validator('months')
+    def months_is_not_empty(cls, v):
+        if v is not None and not v:
+            raise ValueError('If months is set, it must contain values.')
+        return v
 
+    @validator('days')
+    def days_is_not_empty(cls, v):
+        if v is not None and not v:
+            raise ValueError('If days is set, it must contain values.')
+        return v
 
-@validator('days')
-def months__must_contain_values_if_days_are_set(cls, v, values):
-    if 'months' in values and values['months'] is None:
-        raise ValueError('months has to contain values if days are set')
-    return v
+    @validator('days')
+    def months_must_contain_values_if_days_are_set(cls, v, values):
+        if v is not None and 'months' in values and values['months'] is None:
+            raise ValueError('Months has to contain values if days are set.')
+        return v
 
+    @validator('hours')
+    def hours_is_not_empty(cls, v):
+        if v is not None and not v:
+            raise ValueError('If hours is set, it must contain values.')
+        return v
 
-@validator('hours')
-def days__must_contain_values_if_hours_are_set(cls, v, values):
-    if 'days' in values and values['days'] is None:
-        raise ValueError('days has to contain values if hours are set')
-    return v
+    @validator('hours')
+    def days_must_contain_values_if_hours_are_set(cls, v, values):
+        if v is not None and 'days' in values and values['days'] is None:
+            raise ValueError('Days has to contain values if hours are set.')
+        return v
